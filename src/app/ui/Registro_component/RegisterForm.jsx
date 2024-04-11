5c31921c877effdf6ca7064ff92790b4c7e08cc7
GokuPrograming <verafrancomiguel1d@gmail.com>
2024-04-11 03:58:57 -0600

    funcionalidad del registro

--- src/app/ui/Registro_component/RegisterForm.jsx
+++ src/app/ui/Registro_component/RegisterForm.jsx
@@ -1,59 +1,110 @@
-const { default: UserTable } = require("./UserTable");
-
+"use client"
+import React, { useState } from 'react';
+import UserTable from './UserTable'; // Asegúrate de que la ruta sea correcta
+//recibimos los valores del formulario
 function RegisterForm() {
+    const [formData, setFormData] = useState({
+        nombre: '',
+        apellido_paterno: '',
+        apellido_materno: ''
+    });
+// nos ayuda a administrar la actualizacion de la tabla
+    const [refreshTable, setRefreshTable] = useState(false);
+///ayuda a que los datos se ongresen
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        setFormData({
+            ...formData,
+            [name]: value
+        });
+    };
+///evento para cuando se ejecute el registro
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        try {
+            const response = await fetch('http://localhost:3000/usuario/registro', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify(formData)
+            });
+
+            const data = await response.json();
+            console.log(data);
+
+            // Actualizar el estado para refrescar la tabla
+            setRefreshTable(!refreshTable);
+            setFormData({
+                nombre: '',
+                apellido_paterno: '',
+                apellido_materno: ''
+            });
+        } catch (error) {
+            console.error('Error al enviar los datos:', error);
+        }
+    };
     return (
         <>
-        <div className="container mx-auto p-4">
-            <h1 className="text-2xl mb-4">Registrar Usuario</h1>
-            <form className="mb-4">
-                <div className="mb-4">
-                    <label htmlFor="nombre" className="block text-sm font-medium text-gray-600">
-                        Nombre
-                    </label>
-                    <input
-                        type="text"
-                        id="nombre"
-                        name="nombre"
-                        className="mt-1 p-2 w-full border rounded-md"
-                        placeholder="Ingrese su nombre"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="apellidoPaterno" className="block text-sm font-medium text-gray-600">
-                        Apellido Paterno
-                    </label>
-                    <input
-                        type="text"
-                        id="apellidoPaterno"
-                        name="apellidoPaterno"
-                        className="mt-1 p-2 w-full border rounded-md"
-                        placeholder="Ingrese su apellido paterno"
-                    />
-                </div>
-                <div className="mb-4">
-                    <label htmlFor="apellidoMaterno" className="block text-sm font-medium text-gray-600">
-                        Apellido Materno
-                    </label>
-                    <input
-                        type="text"
-                        id="apellidoMaterno"
-                        name="apellidoMaterno"
-                        className="mt-1 p-2 w-full border rounded-md"
-                        placeholder="Ingrese su apellido materno"
-                    />
-                </div>
-                <div className="flex justify-end">
-                    <button
-                        type="submit"
-                        className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    >
-                        Registrar
-                    </button>
-                </div>
-            </form>
-        </div>
-        <UserTable/>
-    </>
+            <div className="container mx-auto p-4">
+                <h1 className="text-2xl mb-4">Registrar Usuario</h1>
+                <form className="mb-4" onSubmit={handleSubmit}>
+                    <div className="mb-4">
+                        <label htmlFor="nombre" className="block text-sm font-medium text-gray-600">
+                            Nombre
+                        </label>
+                        <input
+                            type="text"
+                            id="nombre"
+                            name="nombre"
+                            value={formData.nombre}
+                            onChange={handleChange}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            placeholder="Ingrese su nombre"
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="apellidoPaterno" className="block text-sm font-medium text-gray-600">
+                            Apellido Paterno
+                        </label>
+                        <input
+                            type="text"
+                            id="apellidoPaterno"
+                            name="apellido_paterno"
+                            value={formData.apellido_paterno}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            placeholder="Ingrese su apellido paterno"
+                            onChange={handleChange}
+                        />
+                    </div>
+                    <div className="mb-4">
+                        <label htmlFor="apellidoMaterno" className="block text-sm font-medium text-gray-600">
+                            Apellido Materno
+                        </label>
+                        <input
+                            type="text"
+                            id="apellidoMaterno"
+                            name="apellido_materno"
+                            value={formData.apellido_materno}
+                            onChange={handleChange}
+                            className="mt-1 p-2 w-full border rounded-md"
+                            placeholder="Ingrese su apellido materno"
+                        />
+                    </div>
+                    <div className="flex justify-end">
+                        <button
+                            type="submit"
+                            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+                        >
+                            Registrar
+                        </button>
+                    </div>
+                </form>
+            </div>
+            {/* va a almacenar lo que le mandemos */}
+            <UserTable refreshTable={refreshTable} />
+        </>
     );
 }
 
--- src/app/ui/Registro_component/UserTable.jsx
+++ src/app/ui/Registro_component/UserTable.jsx
@@ -1,28 +1,43 @@
-function UserTable({ users }) {
-    return (
-      <div className="container mx-auto p-4">
-        <h1 className="text-2xl mb-4">Lista de Usuarios</h1>
-        <table className="min-w-full border-collapse border border-gray-300">
-          <thead>
-            <tr>
-              <th className="border border-gray-300 px-4 py-2">Nombre</th>
-              <th className="border border-gray-300 px-4 py-2">Apellido Paterno</th>
-              <th className="border border-gray-300 px-4 py-2">Apellido Materno</th>
+
+"use client"
+import React, { useEffect, useState } from 'react';
+function UserTable({ refreshTable }) {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      const response = await fetch('http://localhost:3000/usuario');
+      const result = await response.json();
+      setData(result.data);
+      console.log(result.data);
+    }
+
+    fetchData();
+  }, [refreshTable]);
+  return (
+
+    <div className="container mx-auto p-4">
+      <h1 className="text-2xl mb-4">Lista de Usuarios</h1>
+      <table className="min-w-full border-collapse border border-gray-300">
+        <thead>
+          <tr>
+            <th className="border border-gray-300 px-4 py-2">Nombre</th>
+            <th className="border border-gray-300 px-4 py-2">Apellido Paterno</th>
+            <th className="border border-gray-300 px-4 py-2">Apellido Materno</th>
+          </tr>
+        </thead>
+        <tbody>
+          {data.map(item => (
+            <tr key={item.id_ingeniero}>
+              <td className="border border-gray-300 px-4 py-2">{item.nombre}</td>
+              <td className="border border-gray-300 px-4 py-2">{item.apellido_paterno}</td>
+              <td className="border border-gray-300 px-4 py-2">{item.apellido_materno}</td>
             </tr>
-          </thead>
-          <tbody>
-           
-              <tr>
-                <td className="border border-gray-300 px-4 py-2"></td>
-                <td className="border border-gray-300 px-4 py-2"></td>
-                <td className="border border-gray-300 px-4 py-2"></td>
-              </tr>
-       
-          </tbody>
-        </table>
-      </div>
-    );
-  }
-  
-  export default UserTable;
-  
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default UserTable;
